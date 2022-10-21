db4a4f1b164a019453075968ccd56b1080307169
andreaceresoli17142 <andrea.ceresoli03@gmail.com>
2022-10-21 14:48:24 +0200

    added basic functionalities

--- main.go
+++ main.go
@@ -5,11 +5,17 @@ import (
 	"image"
 	"image/color"
 	"image/png"
-	"math"
 	"net/http"
+	"strconv"
+	"time"
 )
 
-var res = 1000
+var (
+	res     = 1000
+	zoom    = 0.05
+	xoffset = 0.4
+	yoffset = 0.37
+)
 
 func calcColor(col uint64) color.RGBA {
 
@@ -33,8 +39,18 @@ func pageMain(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte(`
 			<!doctype html>
 			<title>madelbrot set</title>
-			<h1>madelbrot set</h1>
-			<img src="/madelbrot.png">
+
+			<style>
+				#image-id {
+				max-width: 85vw;
+					max-height: 85vh;
+				}
+			</style>
+
+			<center>
+				<h1>madelbrot set</h1>
+				<img src="/madelbrot.png" id="image-id">
+			</center>
 			`))
 }
 
@@ -42,8 +58,71 @@ func uint8Clamper(mini float64, maxi float64, value float64) uint8 {
 	return uint8(mapValue(mini, maxi, 0, 256, value))
 }
 
+func calculate_pixel(img *image.RGBA, res int, x int, y int) {
+
+	//fmt.Println("starting to calculate: ", x, y)
+
+	Ca := float64(mapValue(float64(-res/2), float64(res/2-1), -zoom+xoffset, zoom+xoffset, float64(x)))
+	Cb := float64(mapValue(float64(-res/2), float64(res/2-1), -zoom+yoffset, zoom+yoffset, float64(y)))
+
+	var Za, Zb float64
+
+	var i int
+
+	for Za*Za+Zb*Zb < 4 && i < 1000 {
+		TZa := (Za * Za) - (Zb * Zb) + Ca
+		TZb := (2 * Za * Zb) + Cb
+		Za = TZa
+		Zb = TZb
+		i++
+	}
+
+	col := color.RGBA{
+		R: 0,
+		G: uint8Clamper(0, 1000.0, float64(i)),
+		B: 0,
+		A: 255,
+	}
+
+	img.Set((res/2)+x, (res/2)+y, col)
+	//fmt.Println("finished to calculate: ", x, y)
+}
+
 func pageImage(w http.ResponseWriter, r *http.Request) {
+
+	rest := r.URL.Query().Get("res")
+	rzoom := r.URL.Query().Get("zoom")
+	rxoffs := r.URL.Query().Get("xoffs")
+	ryoffs := r.URL.Query().Get("yoffs")
+
+	if rest == "" || rzoom == "" || rxoffs == "" || ryoffs == "" {
+		return
+	}
+
+	res, err := strconv.Atoi(rest)
+	if err != nil {
+		return
+	}
+
+	zoom, err = strconv.ParseFloat(rzoom, 64)
+	if err != nil {
+		return
+	}
+
+	xoffset, err = strconv.ParseFloat(rxoffs, 64)
+	if err != nil {
+		return
+	}
+
+	yoffset, err = strconv.ParseFloat(ryoffs, 64)
+	if err != nil {
+		return
+	}
+
 	img := image.NewRGBA(image.Rect(0, 0, res, res))
+
+	fmt.Println("starting to generate image")
+	start := time.Now()
 
 	for y := -res / 2; y < res/2; y++ {
 		for x := -res / 2; x < res/2; x++ {
@@ -52,38 +131,22 @@ func pageImage(w http.ResponseWriter, r *http.Request) {
 			/*
 				f(z) = z^2 + C
 			*/
-
-			Ca := float64(mapValue(float64(-res/2), float64(res/2-1), -2.0, 2.0, float64(x)))
-			Cb := float64(mapValue(float64(-res/2), float64(res/2-1), -2.0, 2.0, float64(y)))
-
-			var Za, Zb float64
-
-			var i int
-
-			for Za*Za+Zb*Zb < 4 && i < 1000 {
-				Za = (Za * Za) - (Zb * Zb) + Ca
-				Zb = (2 * Za * Zb) + Cb
-				i++
-			}
-
-			col := color.RGBA{
-				R: 0,
-				G: uint8Clamper(0, math.Sqrt(1000.0), math.Sqrt(float64(i))),
-				B: 0,
-				A: 255,
-			}
-
-			img.Set((res/2)+x, (res/2)+y, col)
+			calculate_pixel(img, res, x, y)
 		}
 		//fmt.Println(y)
 	}
 
 	png.Encode(w, img)
+
+	fmt.Printf("set generation took %v\n", time.Since(start))
 }
 
 func main() {
 	http.HandleFunc("/", pageMain)
 	http.HandleFunc("/madelbrot.png", pageImage)
 	fmt.Println("Listening on http://localhost:3000/")
-	http.ListenAndServe(":3000", nil)
+	err := http.ListenAndServe(":3000", nil)
+	if err != nil {
+		fmt.Printf("An error has occured serving the webpages: %v", err)
+	}
 }
